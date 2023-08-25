e40a19a1458bf7222f0db97f9b73d7c5eff0abfc
Abhay <abhay.johri0@gmail.com>
2023-08-26 01:06:02 +0530

    Completed the functionality of Trigonometric functions

--- trigoLogic.js
+++ trigoLogic.js
@@ -1,7 +1,27 @@
-var refBtns = document.getElementsByClassName('refresh-button');
+var tiles = Array.from(document.getElementsByClassName('tiles'));
 
-Array.from(refBtns).forEach(btn => {
-    btn.addEventListener('click', e => {
-        
-    })
+tiles.forEach((tile) => {
+    let calcBtn = tile.querySelector('.input').querySelector('.refresh-button');
+    calcBtn.addEventListener('click', (calcEvent) => {
+        try{
+            let val = tile.querySelector('.input').querySelector('input').value;
+            let id = tile.querySelector('.output').getAttribute('id');
+            let output = Number(val);
+
+            if(id === 'sine')           output = Math.sin(val);
+            else if(id === 'cosine')    output = Math.cos(val);
+            else if(id === 'tan')       output = Math.tan(val);
+            else if(id === 'cotan')     output = 1/Math.tan(val);
+            else if(id === 'cosec')     output = 1/Math.sin(val);
+            else if(id === 'secant')    output = 1/Math.cos(val);
+            
+
+            tile.querySelector('.output').querySelector('label').innerHTML = output.toFixed(2);
+        }catch(exception){
+            console.log('Could not calculate the value!');
+        }
+    });
+
+    console.log(tile.querySelector('.output').querySelector('.output-label'));
+
 });
